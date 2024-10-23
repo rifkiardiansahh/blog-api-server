21a093d149b1e99b6d5a390c6e2316fb1cada760
RIFKY <rifkiardiansah96@gmail.com>
2024-10-23 16:27:42 +0700

    Feat : Create Blog Post Endpoint

---
+++ src/models/post.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("post", postSchema);
+
+export default Post;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
-// import express from "express";
-// import mongoose from "mongoose";
-
-// const app = express();
-// const port = 3000;
-
-// mongoose.connect(`mongodb://localhost:27017/blogDB`);
-// .then(() => app.listen(port))
-// .then(() => console.log(`Database successfully connected an app listening on port ${port}`))
-// .then(err => console.log('Connection error', err))
-
 import express from "express";
 import mongoose from "mongoose";
 
+import postRoutes from "./routes/post.js";
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/blog", postRoutes);
+
+app.all("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
 
 mongoose
   .connect(`mongodb://localhost:27017/blogDB`)
