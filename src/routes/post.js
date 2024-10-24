7b98dc5ca75e53cb1a4525c7a99540fcea3f55ef
RIFKY <rifkiardiansah96@gmail.com>
2024-10-24 22:47:04 +0700

    Feat : create blog post endpoint validation

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,9 +1,20 @@
 import express from "express";
+import { body } from "express-validator";
 import { createPost } from "../controllers/post.js";
 import { upload } from "../middlewares/upload.js";
+import { validateRequest } from "../middlewares/validateRequest.js";
 
 const router = express.Router();
 
-router.post("/post", upload.single("image"), createPost);
+router.post(
+  "/post",
+  upload.single("image"),
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("body").not().isEmpty().withMessage("Body is required"),
+  ],
+  validateRequest,
+  createPost
+);
 
 export default router;
